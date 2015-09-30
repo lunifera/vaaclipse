c41cc4049757681858c89d7d1af497804d982d3f
Florian Pirchner <florian.pirchner@gmail.com>
2015-09-30 11:14:34 +0200

    Added IUser interface

--- org.semanticsoft.vaaclipse.publicapi/src/org/semanticsoft/vaaclipse/publicapi/authentication/IUser.java
+++ org.semanticsoft.vaaclipse.publicapi/src/org/semanticsoft/vaaclipse/publicapi/authentication/IUser.java
@@ -11,26 +11,15 @@
 package org.semanticsoft.vaaclipse.publicapi.authentication;
 
 /**
- * @author rushan
- *
+ * Every user object needs to implement this interface.
  */
-public class AuthenticationConstants {
+public interface IUser {
 
 	/**
-	 * For e4 context. User id as string.
+	 * Returns the user id.
+	 * 
+	 * @return
 	 */
-	public static final String USER_ID = "userId";
+	String getId();
 
-	/**
-	 * For e4 context. Needs to implement IUser.
-	 */
-	public static final String USER = "user";
-
-	public static class Events {
-
-		public static class Authentication {
-			public static final String name = "vaaclipseAuthentication";
-			public static final String userClass = "userClass";
-		}
-	}
 }
